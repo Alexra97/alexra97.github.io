da8e7034c2f4420cd29f6470b3c296c461195092
Alejandro R. López <alejandrorlopezrodriguez@gmail.com>
2022-11-05 18:16:49 +0100

    Update ScrollObserver.js

--- ScrollObserver.js
+++ ScrollObserver.js
@@ -1,17 +1,15 @@
-$(document).ready(function () {
-  const contenedor_titulo = document.querySelector('.contenedor-titulo');
-  contenedor_titulo.classList.remove('contenedor-titulo-transition');
+const contenedor_titulo = document.querySelector('.contenedor-titulo');
+contenedor_titulo.classList.remove('contenedor-titulo-transition');
 
-  const observer_title = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        contenedor_titulo.classList.add('contenedor-titulo-transition');
-        return;
-      }
+const observer_title = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      contenedor_titulo.classList.add('contenedor-titulo-transition');
+      return;
+    }
 
-      contenedor_titulo.classList.remove('contenedor-titulo-transition');
-    });
+    contenedor_titulo.classList.remove('contenedor-titulo-transition');
   });
+});
 
-  observer_title.observe(document.querySelector('.contenedor-titulo-wrapper'));
-})
+observer_title.observe(document.querySelector('.contenedor-titulo-wrapper'));
